3770ce7969a9584ad10fe411ef49e038a49c5a84
Sam Maes <sammaes92@gmail.com>
2015-03-23 20:00:52 +0100

    Opruimen van alle code...

--- Code/Oude Code/codeSam/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
-
-#define DUMMY 1     // In commentaar zetten als er geen dummy waarden gebruikt moeten worden
-#define DEBUG 1     // In commentaar zetten als er geen debug info naar output geprint moet worden
-
-#endif // MAIN_H_INCLUDED
